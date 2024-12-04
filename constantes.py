d3a384bf7468cad47f241173ca344e57f908f677
Carmen Cespedes <138259773+carmen-cespedes@users.noreply.github.com>
2024-12-03 22:49:13 -0300

    juego preguntados 1

---
+++ constantes.py
@@ -0,0 +1,8 @@
+import pygame
+
+# dimension de pantalla
+ANCHO = 800
+ALTO = 600
+
+DIMENSION_ICONO = (220,60)
+DIMENSION_BOTON_SALIR_RANKING = pygame.Rect(300, 540, 200, 50)
---
+++ settings.py
@@ -0,0 +1,5 @@
+import pygame
+from constantes import *
+
+# Configuración de la ventana
+pantalla = pygame.display.set_mode((ANCHO, ALTO))
